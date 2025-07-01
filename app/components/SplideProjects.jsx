5590bcf1179785e633fcafc372182a15e9c11624
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2025-07-01 22:57:14 +0700

    refactor: enhance SplideProjects component layout and styling

--- app/components/SplideProjects.jsx
+++ app/components/SplideProjects.jsx
@@ -5,7 +5,11 @@ import "@splidejs/react-splide/css";
 // or only core styles
 // import "@splidejs/react-splide/css/core";
 import Link from "next/link";
-import { IconArrowNarrowRight, IconArrowNarrowLeft } from "@tabler/icons-react";
+import {
+	IconArrowNarrowRight,
+	IconArrowNarrowLeft,
+	IconExternalLink,
+} from "@tabler/icons-react";
 
 export default function SplideProjects({ projects }) {
 	return (
@@ -16,20 +20,21 @@ export default function SplideProjects({ projects }) {
 				direction: "ltr",
 				autoplay: true,
 				arrows: false,
+				autoHeight: true,
 			}}
-			className="my-4"
+			className="my-10"
 		>
 			{projects.map((project, index) => (
-				<SplideSlide key={index} className="">
-					<div className="flex flex-col p-4">
+				<SplideSlide key={index} className="p-1 h-full">
+					<div className="flex flex-col grow">
 						<h3 className="text-2xl font-bold">{project.projectName}</h3>
-						<p className="">{project.description}</p>
+						<p className="mt-2">{project.description}</p>
 						<Link
 							href={`${project.previewURL}`}
-							className="mt-4 mr-2 inline-flex bg-waikawa-gray-300 text-obsidian font-bold p-3 md:w-fit rounded-xl"
+							className="mt-4 gap-x-1 flex items-center justify-center md:justify-start bg-waikawa-gray-300 text-obsidian text-lg font-bold p-3 md:w-fit rounded-xl"
 						>
 							Live view
-							<IconArrowNarrowRight size={24} />
+							<IconExternalLink size={24} />
 						</Link>
 					</div>
 				</SplideSlide>
